7593f78bac30a78624c49b3e37c46ef02de9d2f4
Brendan <coffman10012@gmail.com>
2024-01-17 13:12:54 -0500

    Code

---
+++ Lab_2/sampleProgramOne.c
@@ -0,0 +1,12 @@
+#include <stdio.h>
+#include <sys/types.h>
+#include <unistd.h>
+int main()
+{
+    printf("Before fork\n");
+    fork();
+    sleep(10);
+    printf("After fork\n");
+
+    return 0;
+}
