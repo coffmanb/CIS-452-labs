befe550de4ef3d1ed0e6bdc2ca1934025b55b07c
Brendan <coffman10012@gmail.com>
2024-01-18 14:35:57 -0500

    stat for each child

--- Lab_2/sampleProgramFour.c
+++ Lab_2/sampleProgramFour.c
@@ -6,6 +6,16 @@
 #include <stdlib.h>
 #include <string.h>
 
+void getUsageStats(void)
+{
+    struct rusage usage;
+    getrusage(RUSAGE_CHILDREN, &usage);
+
+    printf("CPU time used by the child process: %ld.%06ld seconds\n", 
+        usage.ru_utime.tv_sec, usage.ru_utime.tv_usec);
+    printf("involuntary context switches by the child process: %ld\n", 
+        usage.ru_nivcsw);
+}
 int main(void)
 {
     // Max of 100 characters in input line
@@ -18,14 +28,13 @@ int main(void)
 
     while(strcmp(userInput, "quit") != 0)
     {
-        char* str = strtok(userInput, " ");
-
         /*
-            The following code is from ChatGPT. This line changes the
+            The following code to read the input is from ChatGPT. This line changes the
             newline character at the end of the fgets() string to a NULL
             terminating character. This allows the both params in execvp
             to terminate properly.
         */
+        char* str = strtok(userInput, " ");
         size_t len = strlen(userInput);
         if (len > 0 && userInput[len - 1] == '\n')
         {
@@ -44,7 +53,9 @@ int main(void)
 
         // last element is NULL
         args[count] = NULL;
-        pid_t child, pid;
+
+        
+        pid_t pid;
 
         if(strcmp(userInput, "quit") == 0)
         {
@@ -71,17 +82,11 @@ int main(void)
         else
         {
             wait(&pid);
+            getUsageStats();
             puts("Enter a command to execute");
             char* userInput = fgets(command, max_char, stdin);
         }
     }
 
-    struct rusage usage;
-    getrusage(RUSAGE_CHILDREN, &usage);
-
-    printf("CPU time used by the child process: %ld.%06ld seconds\n", 
-        usage.ru_utime.tv_sec, usage.ru_utime.tv_usec);
-    printf("involuntary context switches by the child process: %ld\n", 
-        usage.ru_nivcsw);
     return 0;
 }
