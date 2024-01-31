445da735f7bf9432ee2a86395fffca498cbbd096
Brendan Coffman <112509868+coffmanb@users.noreply.github.com>
2024-01-31 13:10:53 -0500

    Lab3 (#4)
    lab3

---
+++ Lab_3/communicatingProcesses.c
@@ -0,0 +1,82 @@
+/*****************************************
+Brandon Baker, Brendan Coffman
+Dr. Bobeldyk
+Operating Systems Concepts
+Lab Three Communicating Processes
+******************************************/
+
+#include <stdio.h>
+#include <stdlib.h>
+#include <unistd.h>
+#include <string.h>
+#include <signal.h>
+#include <sys/wait.h>
+
+#define READ 0
+#define WRITE 1
+
+void sigHandler(int);
+void installSigHandlers(void);
+
+int main()
+{
+    int pid;
+
+    pid = fork();
+    if(pid < 0) // Fork failed
+    {
+        perror("Fork failed\n");
+        exit(1);
+    }
+
+
+    if(pid == 0) // Child process
+    { 
+        int random;
+        while(1)
+        {
+            random = 1 + rand()%4;
+            sleep(random);
+            random = rand()%2;
+            kill(getppid(), random ? SIGUSR1 :SIGUSR2);
+        }
+    }
+    else
+    {
+        installSigHandlers();
+        printf("spawned child PID# %d\n", pid);
+        while(1)
+        {
+            printf("waiting...\t");
+            fflush(stdout);
+            pause();
+            installSigHandlers();
+        }
+    }
+
+    return 0;
+}
+void installSigHandlers(void)
+{
+    signal(SIGINT, sigHandler);
+    signal(SIGUSR1, sigHandler);
+    signal(SIGUSR2, sigHandler);
+}
+
+void sigHandler(int sigNum)
+{
+    if(sigNum == SIGINT)
+    {
+        printf(" received shutting down\n");
+        exit(0);
+    }
+    else if(sigNum == SIGUSR1)
+        printf("received a SIGUSR1 signal.\n");
+    else if(sigNum == SIGUSR2)
+        printf("received a SIGUSR2 signal.\n");
+    else
+    {
+        printf("unknown condition exiting");
+        exit(0);
+    }
+}
---
+++ Lab_3/sampleProgramTwo.c
@@ -0,0 +1,56 @@
+/*****************************************
+Brandon Baker, Brendan Coffman
+Dr. Bobeldyk
+Operating Systems Concepts
+Lab Three SampleProgramTwo Modified Code
+******************************************/
+
+#include <stdio.h>
+#include <stdlib.h>
+#include <unistd.h>
+#include <string.h>
+
+#define READ 0
+#define WRITE 1
+
+int main()
+{
+    int fd[2];
+    int pipeCreationResult;
+    int pid;
+    char myStringOutput[] = "This a test!";
+    char myStringInput[50];
+
+    pipeCreationResult = pipe(fd);
+
+    if(pipeCreationResult < 0)
+    {
+        perror("Failed pipe creation\n");
+        exit(1);
+    }
+    pid = fork();
+    if(pid < 0) // Fork failed
+    {
+        perror("Fork failed");
+        exit(1);
+    }
+
+    int output = 3;
+    int input;
+    int bytesRead;
+
+    if(pid == 0) // Child process
+    { 
+        close(fd[0]);
+        write(fd[1], &myStringOutput, strlen(myStringOutput) + 1);
+        printf("Child wrote [%s]\n", myStringOutput);
+    }
+    else
+    {
+        close(fd[1]);
+        bytesRead = read(fd[0], &myStringInput, 50);
+        printf("Parent received [%s] from child process\n%d bytes were read\n", myStringInput, bytesRead);
+    }
+    
+    return 0;
+}
