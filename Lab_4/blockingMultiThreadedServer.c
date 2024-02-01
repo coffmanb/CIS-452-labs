e7b49cbfe07ecdd4c5ef33f11df391867c30cdc0
Brendan <coffman10012@gmail.com>
2024-02-01 13:00:47 -0500

    done?

--- Lab_4/blockingMultiThreadedServer.c
+++ Lab_4/blockingMultiThreadedServer.c
@@ -6,42 +6,69 @@
 #include <string.h>
 #include <signal.h>
 
-#define MAX_NUM_THREADS 10
-void sigHandler(int);
-void* readFile(void* arg);
+#define MAX_NUM_THREADS 3
+
+
+typedef struct{
+    char filename[100];
+    int status; //0 is free 1 is running
+}threadStatus_t;
 
 pthread_t threads[MAX_NUM_THREADS];
-int threadStatus[MAX_NUM_THREADS];
+threadStatus_t threadStatus[MAX_NUM_THREADS];
 
-int main(){
-    
-    
-    void *result1, *result2;
-    int threadStatus1, threadStatus2;
-    int joinStatus1, joinStatus2;
+
+void sigHandler(int);
+void* readFile(void* arg);
+int findAvailableThread(threadStatus_t *threadList);
+
+
+int main() {
+    signal(SIGINT, sigHandler);  // Set up signal handler for SIGINT
+
     char fileName[100];
-    while(1)
-    {
+    while (1) {
         int thread = 0;
         printf("Enter a file name: ");
         fflush(stdout);
-        scanf("%s",fileName);
+        scanf("%s", fileName);
 
-        //Find next available thread
-        while(threadStatus[thread] == 1)
+        thread = findAvailableThread(threadStatus);
+
+        threadStatus[thread].status = 1;  // Mark thread as in use
+        strcpy(threadStatus[thread].filename, fileName);
+        pthread_create(&threads[thread], NULL, readFile, &threadStatus[thread]);
+        for(int i = 0; i < MAX_NUM_THREADS; i++)
         {
-            thread++;
+            printf("thread %d: %d\n", i, threadStatus[i].status);
         }
-        threadStatus1 = pthread_create (&threads[thread], NULL, readFile, fileName);
     }
+
     return 0;
 }
 
-void* readFile(void *myArgument){
-    char *myPtr = (char *)myArgument;
+int findAvailableThread(threadStatus_t *threadList)
+{
+    int thread = 0;
+    while (threadList[thread].status == 1) {
+        thread++;
+        if (thread >= MAX_NUM_THREADS) {
+            printf("Maximum number of threads reached.\n");
+            sleep(1);
+            return findAvailableThread(threadList);
+        }
+    }
+    return thread;
+}
+
+void* readFile(void* myArgument)
+{
+    threadStatus_t* threadInfo = (threadStatus_t*)myArgument;
     int sleepTime = rand() % 5;
-    sleep(sleepTime == 0 ? 6 + rand() % 5 : 1);
-    printf("Found: %s\n", myPtr);
+    sleep((sleepTime == 0 ? 6 + rand() % 5 : 1));
+    printf("Found: %s\n", threadInfo->filename);
+    fflush(stdout);
+    threadInfo->status = 0;
     return NULL;
 }
 
@@ -51,14 +78,16 @@ void sigHandler(int sigNum)
     {
         printf(" received shutting down\n");
         int thread = 0;
-        //shut down running threads
+        // Shut down running threads
         for(int i = 0; i < MAX_NUM_THREADS; i++)
         {
-            if (threadStatus[i] == 1)
+            printf("thread %d: %d", i, threadStatus[i].status);
+            if(threadStatus[i].status == 1)
             {
-                pthread_join(&threads[i],NULL);
+                pthread_join(threads[i], NULL);
             }
         }
+        printf("All tasks done!\n");
         exit(0);
     }
 }
