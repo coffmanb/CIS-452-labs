fe3f6a41fe55898a4cf875c98074a2a4e5c1e7d1
Brendan Coffman <112509868+coffmanb@users.noreply.github.com>
2024-02-09 10:07:25 -0500

    Lab5 (#7)
    lab5

---
+++ Lab_5/sampleProgramOne.c
@@ -0,0 +1,53 @@
+/*****************************************
+Brandon Baker, Brendan Coffman
+Dr. Bobeldyk
+Operating Systems Concepts
+Lab Five sampleProgramOne.c
+
+Modified sampleProgramOne after completion
+******************************************/
+
+#include <stdio.h>
+#include <stdlib.h>
+#include <sys/types.h>
+#include <sys/stat.h>
+#include <sys/ipc.h>
+#include <sys/shm.h>
+#include <unistd.h>
+
+#define FOO 4096
+int main ()
+{
+    int shmId;
+    char *sharedMemoryPtr;
+
+    if((shmId = shmget(IPC_PRIVATE, FOO, IPC_CREAT|S_IRUSR|S_IWUSR)) < 0) {
+        perror ("Unable to get shared memory\n");
+        exit (1);
+    }
+    if((sharedMemoryPtr = shmat (shmId, 0, 0)) == (void*) -1) {
+        perror ("Unable to attach\n");
+        exit (1);
+    }
+
+    printf("ID of segment: %d\n", shmId);
+    pause();
+
+    // Changes for Q4
+    struct shmid_ds shm_info;
+    if (shmctl(shmId, IPC_STAT, &shm_info) < 0) {
+        perror("Unable to get shared memory information\n");
+        exit(1);
+    }
+    printf("Shared memory segment size: %ld bytes\n", shm_info.shm_segsz);
+
+    if(shmdt (sharedMemoryPtr) < 0) {
+        perror ("Unable to detach\n");
+        exit (1);
+    }
+    if(shmctl (shmId, IPC_RMID, 0) < 0) {
+        perror ("Unable to deallocate\n");
+        exit(1);
+    }
+    return 0;
+}
